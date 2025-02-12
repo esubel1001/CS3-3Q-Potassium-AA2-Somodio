b6e8034dc4666a8ad821c188d750d9f4244796fe
esubel1001 <ayssomodio22@src.pshs.edu.ph>
2025-02-12 14:23:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function exponential () {
     let rateOfGrowth = parseFloat (prompt("Enter the value of rate of growth: "));
     let timeInHours = parseFloat(prompt("Enter the value of time in hours: "));
 
-    let finalPopulation = Math.round(initialPopulation * Math.pow(Math.E, (rateOfGrowth*timeInHours)));
+    let finalPopulation = Math.round(initialPopulation * Math.pow(Math.E, (rateOfGrowth * timeInHours)));
 
     let locationOfMonster = prompt("Enter location of the monster: ");
     let nameOfMonster = prompt("Enter name of the monster: ");
-
     let nameAndLocationOfMonster = locationOfMonster.concat(" ", nameOfMonster).toUpperCase();
 
     document.getElementById("result").innerHTML = "  After  " + timeInHours + "  hours, the population of  " + nameAndLocationOfMonster + "  has risen to  " + finalPopulation;
